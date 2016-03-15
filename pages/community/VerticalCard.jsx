993282bd323a795e870a0f60d8601dc7b3005c0d
ScottDowne <scott.downe@gmail.com>
2016-03-15 16:33:57 -0400

    horizontal alignment of elements

--- pages/community/VerticalCard.jsx
+++ pages/community/VerticalCard.jsx
@@ -5,6 +5,7 @@ var VerticalCard = React.createClass({
   render: function() {
     return (
       <Illustration
+        height={259} width={460}
         src1x={this.props.imgSrc1x}
         src2x={this.props.imgSrc2x}
         className="vertical-layout"
